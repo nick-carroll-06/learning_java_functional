785a1510a372f911969145a36751d9c6cbd6fcd2
Nick Carroll <ncarroll179@gmail.com>
2025-07-11 11:35:34 -0400

    Initial commit

---
+++ src/main/java/carroll/nick/Functional/FunctionalApplication.java
@@ -0,0 +1,13 @@
+package carroll.nick.Functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carroll/nick/Functional/FunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package carroll.nick.Functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
